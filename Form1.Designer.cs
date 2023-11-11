4aa6baf4e3445974f2496bc346d916e4e4e4bbd4
tzzz121 <104672389+tzzz121@users.noreply.github.com>
2023-11-11 17:10:37 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿
+namespace 期末
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.video = new System.Windows.Forms.Button();
+            this.identify = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // video
+            // 
+            this.video.Location = new System.Drawing.Point(38, 27);
+            this.video.Name = "video";
+            this.video.Size = new System.Drawing.Size(112, 34);
+            this.video.TabIndex = 0;
+            this.video.Text = "讀影片";
+            this.video.UseVisualStyleBackColor = true;
+            this.video.Click += new System.EventHandler(this.video_Click);
+            // 
+            // identify
+            // 
+            this.identify.Location = new System.Drawing.Point(38, 77);
+            this.identify.Name = "identify";
+            this.identify.Size = new System.Drawing.Size(112, 34);
+            this.identify.TabIndex = 1;
+            this.identify.Text = "辨識";
+            this.identify.UseVisualStyleBackColor = true;
+            this.identify.Click += new System.EventHandler(this.identify_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(38, 131);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(482, 390);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(589, 131);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(264, 110);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 533);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.identify);
+            this.Controls.Add(this.video);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button video;
+        private System.Windows.Forms.Button identify;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace 期末
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void video_Click(object sender, EventArgs e)
+        {
+            //Cv2.NamedWindow("Video", WindowFlags.Normal);//產生視窗
+            FrameSource frame = Cv2.CreateFrameSource_Video(@"車牌辨識verification.mp4");
+            Mat mat = new Mat();
+            Mat BlackWhiteVideo = new Mat();
+
+            while (true) {          
+                frame.NextFrame(mat);
+                if (mat.Empty())//播完
+                    break;
+                Cv2.CvtColor(mat, BlackWhiteVideo, ColorConversionCodes.BGR2GRAY);//灰階
+                Cv2.GaussianBlur(BlackWhiteVideo, BlackWhiteVideo, new OpenCvSharp.Size(7, 7), 0, 0);//高斯
+                //邊緣
+                Cv2.Sobel(BlackWhiteVideo, BlackWhiteVideo, MatType.CV_8U, 1, 0, 1);
+                Cv2.Canny(BlackWhiteVideo, BlackWhiteVideo, 100, 100);
+                //二值化
+                Cv2.Threshold(BlackWhiteVideo, BlackWhiteVideo, 0, 255, ThresholdTypes.Binary);
+                //腐蝕和擴張
+                Mat element = Cv2.GetStructuringElement(MorphShapes.Rect, new OpenCvSharp.Size(25, 25), new OpenCvSharp.Point(-1, -1));//閉運算(先擴張後侵蝕)
+                Cv2.MorphologyEx(BlackWhiteVideo, BlackWhiteVideo, MorphTypes.Close, element);
+
+                Mat label = new Mat();
+                Mat xywh = new Mat();
+                Mat c_xy = new Mat();//中心點座標
+                int[,] boxsite = new int[1000, 1000];
+                int num_label = Cv2.ConnectedComponentsWithStats(BlackWhiteVideo, label, xywh, c_xy);
+                
+                for (int i = 0; i < num_label; i++)
+                {
+                    boxsite[i, 0] = xywh.At<int>(i, 0);//起始X
+                    boxsite[i, 1] = xywh.At<int>(i, 1);//起始Y
+                    boxsite[i, 2] = xywh.At<int>(i, 2);//寬
+                    boxsite[i, 3] = xywh.At<int>(i, 3);//長
+
+                    if (boxsite[i, 0] == 0)//沒有讀取到框
+                        continue;
+                    //抓取框
+                    if ((boxsite[i, 2] / boxsite[i, 3]) > 1.5 && (boxsite[i, 2] / boxsite[i, 3]) < 5 && (boxsite[i, 2] * boxsite[i, 3]) > 5000) 
+                    {
+                        Cv2.Rectangle(BlackWhiteVideo, new OpenCvSharp.Point(boxsite[i, 0], boxsite[i, 1]), new OpenCvSharp.Point(boxsite[i, 0] + boxsite[i, 2], boxsite[i, 1] + boxsite[i, 3]), Scalar.White, 1);
+                        Rect rect1 = new Rect(boxsite[i, 0], boxsite[i, 1], boxsite[i, 2], boxsite[i, 3]);
+                        Mat re = new Mat(mat,rect1);//車牌
+                        Mat re2 = new Mat();
+                        Cv2.CvtColor(re, re2, ColorConversionCodes.BGR2GRAY);//灰階
+                        Cv2.GaussianBlur(BlackWhiteVideo, BlackWhiteVideo, new OpenCvSharp.Size(7, 7), 0, 0);//高斯
+                        //二值化
+                        Cv2.Threshold(re2, re2, 140, 255, ThresholdTypes.Binary); 
+                        pictureBox2.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(re2);
+                    }
+                }
+                
+                //Cv2.ImShow("Video", mat);//顯示影片
+                if (Cv2.WaitKey(30) == 27){//將程式停下
+                    break;
+                }
+                pictureBox1.Image = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(mat);
+            }
+            
+        }
+
+        private void identify_Click(object sender, EventArgs e)
+        {
+            Mat mat = new Mat();
+            Mat temp = new Mat();
+            Cv2.CvtColor(mat, temp, ColorConversionCodes.BGR2BGRA);//灰階
+        }
+    }
+}
